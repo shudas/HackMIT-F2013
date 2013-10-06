3e4752b393ec92b6d7c499db455f98b81d3b2000
vikas <skvikas@umich.edu>
2013-10-05 20:58:23 -0400

    All the shits are being added

--- Facebook/getShowsFromFacebook.py
+++ Facebook/getShowsFromFacebook.py
@@ -1,25 +1,28 @@
 import facebook
 import json
 from flask_oauth import OAuth
-app.config['SOCIAL_FACEBOOK'] = {
-    'consumer_key': 'facebook app id',
-    'consumer_secret': 'facebook app secret'
-}
-def makeThemLogin():
-	facebook = oauth.remote_app('facebook',
-    base_url='https://graph.facebook.com/',
-    request_token_url=None,
-    access_token_url='/oauth/access_token',
-    authorize_url='https://www.facebook.com/dialog/oauth',
-    consumer_key=FACEBOOK_APP_ID,
-    consumer_secret=FACEBOOK_APP_SECRET,
-    request_token_params={'scope': 'email'})
-	return facebook['access_token_url']
+import flask
+# app.config['SOCIAL_FACEBOOK'] = {
+#     'consumer_key': 'facebook app id',
+#     'consumer_secret': 'facebook app secret'
+# }
+# FACEBOOK_APP_ID = '188477911223606'
+# FACEBOOK_APP_SECRET = '621413ddea2bcc5b2e83d42fc40495de'
+
 def getShowsFromFacebook():
+	oauth = OAuth()
+	facebooklogin = oauth.remote_app('facebook',
+		base_url='https://graph.facebook.com/',
+		request_token_url=None,
+		access_token_url='/oauth/access_token',
+		authorize_url='https://www.facebook.com/dialog/oauth',
+		consumer_key='104980197484',
+		consumer_secret='848e655bf22e44cad1581dab9a1937b5',
+		request_token_params={'scope': 'email'})
 	#make them login
 	#Make Graph API work
 	# oauth_access_token = 'CAACEdEose0cBANAwO4Cz4owvnRidPVjZBpdbgj5Lu1QYZAw9q8oJFSpvLW35xCcZCPxYpr0dWYTuZCRMFiwR6dx4xuVHc2QpNHpsPpD3ZA3ZAfTTahAarxOOy5wlCxaH8CCxZCNGoKse8ORO186Ak1MpxAMKPEJhL7Y7xHZCvV1Sx87K56srU8D0GGbTmKEYdvNcULHsbxZBiAgZDZD'
-	oauth_access_token = makeThemLogin()
+	oauth_access_token = facebooklogin.get('access_token_url')
 	graph = facebook.GraphAPI(oauth_access_token)
 	# GET https://graph.facebook.com/me?fields=name,birthday,photos.limit(10).fields(id, picture)
 	profile = graph.get_object("me")
